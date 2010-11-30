37a54c9efcd378943df278553730f81149658aea
Henri Bergius <henri.bergius@iki.fi>
2010-11-30 11:43:59 +0200

    Started working on the application browser

---
+++ controllers/index.php
@@ -0,0 +1,12 @@
+<?php
+class com_meego_packages_controllers_index
+{
+    public function __construct(midgardmvc_core_request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function get_index(array $args)
+    {
+    }
+}
