20e9017c763195849548b737be402813b05b6977
Henri Bergius <henri.bergius@iki.fi>
2010-11-30 16:05:44 +0200

    Stubs for displaying package in all repos, all package versions in a repo

--- controllers/package.php
+++ controllers/package.php
@@ -6,6 +6,96 @@ class com_meego_packages_controllers_package
         $this->request = $request;
     }
 
+    public function get_package(array $args)
+    {
+        $qb = com_meego_package::new_query_builder();
+        $qb->add_constraint('name', '=', $args['package']);
+        $packages = $qb->execute();
+        if (count($packages) == 0)
+        {
+            throw new midgardmvc_exception_notfound("Package not found");
+        }
+
+        $this->data['package'] = $packages[0];
+        if (empty($this->data['package']->title))
+        {
+            $this->data['package']->title = $this->data['package']->name;
+        }
+
+        $repositories = array();
+        $this->data['packages'] = array();
+        foreach ($packages as $package)
+        {
+            if (empty($package->title))
+            {
+                $package->title = $package->name;
+            }
+
+            if (!isset($repositories[$package->repository]))
+            {
+                $repository = new com_meego_repository();
+                $repository->get_by_id($package->repository);
+                $repositories[$package->repository] = $repository;
+            }
+
+            $package->repositoryobject = $repositories[$package->repository];
+
+            $package->localurl = midgardmvc_core::get_instance()->dispatcher->generate_url
+            (
+                'package_instance',
+                array
+                (
+                    'package' => $package->name,
+                    'version' => $package->version,
+                    'repository' => $repositories[$package->repository]->name,
+                ),
+                $this->request
+            );
+            $this->data['packages'][] = $package;
+        }
+    }
+
+    public function get_repository(array $args)
+    {
+        $qb = com_meego_package::new_query_builder();
+        $qb->add_constraint('name', '=', $args['package']);
+        $qb->add_constraint('repository.name', '=', $args['repository']);
+        $packages = $qb->execute();
+        if (count($packages) == 0)
+        {
+            throw new midgardmvc_exception_notfound("Package not found");
+        }
+
+        $this->data['package'] = $packages[0];
+
+        if (empty($this->data['package']->title))
+        {
+            $this->data['package']->title = $this->data['package']->name;
+        }
+
+        $this->data['packages'] = array();
+        foreach ($packages as $package)
+        {
+            if (empty($package->title))
+            {
+                $package->title = $package->name;
+            }
+
+            $package->localurl = midgardmvc_core::get_instance()->dispatcher->generate_url
+            (
+                'package_instance',
+                array
+                (
+                    'package' => $package->name,
+                    'version' => $package->version,
+                    'repository' => $args['repository'],
+                ),
+                $this->request
+            );
+            $this->data['packages'][] = $package;
+        }
+    }
+
     public function get_instance(array $args)
     {
         $qb = com_meego_package::new_query_builder();
