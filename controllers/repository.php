93e39c9c6fea06c1d3ba9b9b8f1368ef67bb4205
Henri Bergius <henri.bergius@iki.fi>
2010-11-30 12:09:37 +0200

    Very initial repository list and view

---
+++ controllers/repository.php
@@ -0,0 +1,58 @@
+<?php
+class com_meego_packages_controllers_repository
+{
+    public function __construct(midgardmvc_core_request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Redirect user to correct repository
+     */
+    public function get_redirect(array $args)
+    {
+        // Fallback, didn't recognize browser's MeeGo version, redirect to list of repositories
+        midgardmvc_core::get_instance()->head->relocate
+        (
+            midgardmvc_core::get_instance()->dispatcher->generate_url
+            (
+                'repositories', array(),
+                $this->request
+            )
+        );
+    }
+
+    public function get_index(array $args)
+    {
+        $this->data['repositories'] = array();
+        $qb = com_meego_repository::new_query_builder();
+        $qb->add_constraint('disabledownload', '=', false);
+        $repositories = $qb->execute();
+        foreach ($repositories as $repository)
+        {
+            $repository->localurl = midgardmvc_core::get_instance()->dispatcher->generate_url
+            (
+                'repository', 
+                array
+                (
+                    'repository' => $repository->name,
+                ),
+                $this->request
+            );
+            $this->data['repositories'][] = $repository;
+        }
+    }
+
+    public function get_repository(array $args)
+    {
+        $qb = com_meego_repository::new_query_builder();
+        $qb->add_constraint('disabledownload', '=', false);
+        $qb->add_constraint('name', '=', $args['repository']);
+        $repositories = $qb->execute();
+        if (count($repositories) == 0)
+        {
+            throw new midgardmvc_exception_notfound("Repository not found");
+        }
+        $this->data['repository'] = $repositories[0];
+    }
+}
