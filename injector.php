b6e91adba9b695b58f8cbce5f0f7947d98602918
Henri Bergius <henri.bergius@iki.fi>
2011-03-16 10:50:44 +0200

    Inject Packages-specific templating elements to the style

--- injector.php
+++ injector.php
@@ -14,6 +14,9 @@ class com_meego_packages_injector
         // We inject the template to provide MeeGo styling
         $request->add_component_to_chain($this->mvc->component->get('com_meego_packages'), true);
         $this->request = $request;
+        
+        // Default title for Packages pages, override in controllers when possible
+        midgardmvc_core::get_instance()->head->set_title('MeeGo Packages');
     }
 
     /**
@@ -22,6 +25,12 @@ class com_meego_packages_injector
     public function inject_template(midgardmvc_core_request $request)
     {
         $route = $request->get_route();
+
+        // Replace the default MeeGo sidebar with our own
+        $route->template_aliases['content-sidebar'] = 'cmp-show-sidebar';
+        
+        // Add the CSS and JS files needed by Packages
+        $this->add_head_elements();
 
         $request->set_data_item('admin', false);
 
@@ -39,6 +48,38 @@ class com_meego_packages_injector
                 $request->set_data_item('category_admin_url', $category_admin_url);
             }
         }
+    }
+    
+    private function add_head_elements()
+    {
+        midgardmvc_core::get_instance()->head->add_jsfile(MIDGARDMVC_STATIC_URL . '/eu_urho_widgets/js/jquery.rating/jquery.rating.pack.js');
+        midgardmvc_core::get_instance()->head->add_link
+        (
+            array
+            (
+                'rel' => 'stylesheet',
+                'type' => 'text/css',
+                'href' => MIDGARDMVC_STATIC_URL . '/com_meego_packages/css/packages.css'
+            )
+        );
+        midgardmvc_core::get_instance()->head->add_link
+        (
+            array
+            (
+                'rel' => 'stylesheet',
+                'type' => 'text/css',
+                'href' => MIDGARDMVC_STATIC_URL . '/eu_urho_widgets/js/jquery.rating/jquery.rating.css'
+            )
+        );
+        midgardmvc_core::get_instance()->head->add_link
+        (
+            array
+            (
+                'rel' => 'stylesheet',
+                'type' => 'text/css',
+                'href' => MIDGARDMVC_STATIC_URL . '/midgardmvc-static/com_meego_ratings/css/cmr-ratings.css'
+            )
+        );
     }
 }
 ?>
