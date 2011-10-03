95126ff8b5b85910574e9f69735c76f8bea192ba
Ferenc Szekely <ferenc.szekely@gmail.com>
2011-10-03 17:15:24 +0300

    new skin and its bits and pieces

---
+++ static/js/main.js
@@ -0,0 +1,210 @@
+
+ua = navigator.userAgent.toLowerCase();
+isIE = ((ua.indexOf("msie") != -1) );
+isIE6 = ((ua.indexOf("msie 6.0") != -1) );
+isIE7 = ((ua.indexOf("msie 7.0") != -1) );
+isOPERA = ((ua.indexOf("opera") != -1) );
+isFF = ((ua.indexOf("firefox") != -1) );
+isSafari = ((ua.indexOf("safari") != -1) );
+
+
+jQuery(document).ready(function(){
+
+
+                jQuery('.login  ul li:last').addClass('last');
+                jQuery('nav ul li:first').addClass('first');
+
+
+});
+
+function popup() {jQuery("#popup_comment").modal({
+
+                                opacity:95,
+                                overlayCss: {backgroundColor:"#fff"},
+                                onOpen: function (dialog) {
+                                dialog.overlay.fadeIn('fast', function () {
+                                dialog.data.hide();
+                                dialog.container.fadeIn('fast', function () {
+                                dialog.data.fadeIn('fast');
+                                });
+                                });
+                                },
+                                onClose: function (dialog) {
+                                dialog.data.fadeOut('fast', function () {
+                                dialog.container.hide('fast', function () {
+                                dialog.overlay.hide('fast', function () {
+                                jQuery.modal.close();
+                                });
+                                });
+                                });
+                                }
+                })};
+
+                function popup_ux() {jQuery("#popup_ux").modal({
+
+                                opacity:95,
+                                overlayCss: {backgroundColor:"#fff"},
+                                onOpen: function (dialog) {
+                                dialog.overlay.fadeIn('fast', function () {
+                                dialog.data.hide();
+                                dialog.container.fadeIn('fast', function () {
+                                dialog.data.fadeIn('fast');
+                                });
+                                });
+                                },
+                                onClose: function (dialog) {
+                                dialog.data.fadeOut('fast', function () {
+                                dialog.container.hide('fast', function () {
+                                dialog.overlay.hide('fast', function () {
+                                jQuery.modal.close();
+                                });
+                                });
+                                });
+                                }
+                })};
+
+                function popup_ver() {jQuery("#popup_ver").modal({
+
+                                opacity:95,
+                                overlayCss: {backgroundColor:"#fff"},
+                                onOpen: function (dialog) {
+                                dialog.overlay.fadeIn('fast', function () {
+                                dialog.data.hide();
+                                dialog.container.fadeIn('fast', function () {
+                                dialog.data.fadeIn('fast');
+                                });
+                                });
+                                },
+                                onClose: function (dialog) {
+                                dialog.data.fadeOut('fast', function () {
+                                dialog.container.hide('fast', function () {
+                                dialog.overlay.hide('fast', function () {
+                                jQuery.modal.close();
+                                });
+                                });
+                                });
+                                }
+                })};
+
+                function popup_poll() {jQuery("#popup_poll").modal({
+
+                                opacity:95,
+                                overlayCss: {backgroundColor:"#fff"},
+                                onOpen: function (dialog) {
+                                dialog.overlay.fadeIn('fast', function () {
+                                dialog.data.hide();
+                                dialog.container.fadeIn('fast', function () {
+                                dialog.data.fadeIn('fast');
+                                });
+                                });
+                                },
+                                onClose: function (dialog) {
+                                dialog.data.fadeOut('fast', function () {
+                                dialog.container.hide('fast', function () {
+                                dialog.overlay.hide('fast', function () {
+                                jQuery.modal.close();
+                                });
+                                });
+                                });
+                                }
+                })};
+
+                    function popup_login() {jQuery("#popup_login").modal({
+
+                                opacity:95,
+                                overlayCss: {backgroundColor:"#fff"},
+                                onOpen: function (dialog) {
+                                dialog.overlay.fadeIn('fast', function () {
+                                dialog.data.hide();
+                                dialog.container.fadeIn('fast', function () {
+                                dialog.data.fadeIn('fast');
+                                });
+                                });
+                                },
+                                onClose: function (dialog) {
+                                dialog.data.fadeOut('fast', function () {
+                                dialog.container.hide('fast', function () {
+                                dialog.overlay.hide('fast', function () {
+                                jQuery.modal.close();
+                                });
+                                });
+                                });
+                                }
+                })};
+
+
+//-----------------------------------------------
+function debug(txt)
+{
+try {
+    console.debug(txt);
+    }
+catch(e){}
+}
+//-----------------------------------------------
+function inittabs(selector)
+{
+jQuery(selector).tabs();
+
+jQuery(selector).children('ul').localScroll({
+    target:".tab-set",
+    duration:0,
+    hash:true
+  });
+}
+//-----------------------------------------------
+function initiconmenu(id)
+{
+var mainelement=jQuery('#'+id);
+
+jQuery(mainelement).find('.icons li a').click( function(event)
+    {
+    //event.preventDefault();
+
+    jQuery(mainelement).find('.icons li a').removeClass('active');
+
+    jQuery(this).addClass('active');
+
+    refreshtab();
+    });
+
+refreshtab();
+
+function refreshtab()
+    {
+    var index= jQuery(mainelement).find('.icons li a').index( jQuery(mainelement).find('.icons li a.active') );
+
+    jQuery(mainelement).find('.content').css('display', 'none');
+    jQuery(mainelement).find('.content').eq(index).css('display', 'block');
+    }
+//END;
+}
+
+//-----------------------------------------------
+
+jQuery('.login ul:first > li').each( function()
+    {
+    var min=jQuery(this).width();
+
+    cnt++;
+    sum+=min;
+
+    jQuery(this).find('ul').each(function()
+        {
+        min=Math.max( jQuery(this).width(), min );
+
+        jQuery(this).find('li').each(function()
+            {
+            min=Math.max( jQuery(this).width(), min );
+            });
+
+        });
+
+    jQuery(this).find('ul').css('width', min+'px');
+    jQuery(this).find('ul li').css('width', min+'px');
+
+    jQuery(this).find('ul li.last span.r').css('width', min-10+'px');
+    });
+
+//-----------------------------------------------
+
