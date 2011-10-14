3ff07c07fbc8d19bc79dde5fb83499a8b040ba01
Ferenc Szekely <ferenc.szekely@gmail.com>
2011-10-14 14:16:20 +0300

    hardcode review check for now; update the package's score field if the 1st answer in the form is a boolean true

--- workflow/action/distillreview.php
+++ workflow/action/distillreview.php
@@ -4,39 +4,47 @@ class com_meego_packages_workflow_action_distillreview implements ezcWorkflowSer
     public function execute(ezcWorkflowExecution $execution)
     {
         $form = midgardmvc_ui_forms_generator::get_by_guid($execution->getVariable('review_form'));
-        $review = midgardmvc_ui_forms_store::load_form($form, new midgardmvc_ui_forms_form_instance($execution->getVariable('review')));
+        $instance = new midgardmvc_ui_forms_form_instance($execution->getVariable('review'));
+        $review = midgardmvc_ui_forms_store::load_form($form, $instance);
 
         $items = $form->items;
         $boolean_count = 0;
         $positive = 0;
+
+        $package = new com_meego_package($instance->relatedobject);
+
         foreach ($items as $key => $item)
         {
-            if (!$item instanceof midgardmvc_helper_forms_field_boolean)
+
+            $field = new midgardmvc_ui_forms_form_field($key);
+
+            // ugly hardcoded check, but what can we do..
+            if ($field->title == "Should the application be in this application catalog?")
             {
+                // if the answer belongs to a certain field then we process
+                // this field must be a boolean too
+                if (   $item instanceof midgardmvc_helper_forms_field_boolean
+                    && $item->get_value())
+                {
+                    // add +1 to the package score
+                    $package->metadata->score++;
+                    $res = $package->update();
+
+                    if (! $res)
+                    {
+                        //update failed; do what?
+                    }
+                }
+            }
+            else
+            {
+                // otherwise
                 continue;
             }
-
-            $boolean_count++;
-
-            if ($item->get_value())
-            {
-                $positive++;
-            }
         }
 
-        if (!$boolean_count)
-        {
-            // No booleans in form, distill always true
-            $execution->setVariable('distilled_review', true);
-            return;
-        }
-
-        if ($positive >= $boolean_count / 2)
-        {
-            $execution->setVariable('distilled_review', true);
-            return;
-        }
-        $execution->setVariable('distilled_review', false);
+        // pass on the score
+        $execution->setVariable('distilled_review', $package->metadata->score);
     }
 
     public function __toString()
