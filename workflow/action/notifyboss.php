933343d405430fb549de9f96a256b7f0ee489fea
Ferenc Szekely <ferenc.szekely@gmail.com>
2011-10-14 14:18:27 +0300

    skeleton

--- workflow/action/notifyboss.php
+++ workflow/action/notifyboss.php
@@ -3,8 +3,29 @@ class com_meego_packages_workflow_action_notifyboss implements ezcWorkflowServic
 {
     public function execute(ezcWorkflowExecution $execution)
     {
+        // tell BOSS
+
+        // value will be the latest score of the package
         $value = $execution->getVariable('distilled_review'); // boolean yes or no
-        // TODO: Tell BOSS
+
+        $storage = new midgard_query_storage('com_meego_package_details');
+
+        // get the detailed package object
+        $q = new midgard_query_select($storage);
+
+        $q->set_constraint(new midgard_query_constraint(
+            new midgard_query_property('packageguid'),
+            '=',
+            new midgard_query_value($execution->getVariable('package_instance'))
+        ));
+
+        $q->execute();
+        $package = $q->list_objects();
+
+        if (count($package))
+        {
+            // $package[0] could be used to inform BOSS
+        }
         return;
     }
 
